98c884c7997a922a4ecc7c66638c1b2b64782d0e
Thiago Lino <thiagolinoz@gmail.com>
2019-09-14 11:38:21 -0300

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -60,3 +60,5 @@ gem 'devise', '~> 4.6', '>= 4.6.2'
 gem 'bootstrap', '~> 4.3.1'
 
 gem 'jquery-rails'
+
+gem 'linoz_view_tool', git: 'https://github.com/thiagolinoz/linoz_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       		content_tag(:p, greeting, class: "source-greeting")
     	end
 	end
+
+  def copyright_generator
+    LinozViewTool::Renderer.copyright 'Thiago Lino', 'All rights reserved'
+  end
 end
